09a99108292a713cdd2a44f9643890915b46ebb8
Yerim Moon <myr9901@gmail.com>
2023-05-17 14:22:45 -0700

    changed bookmark css

--- public/js/bookmark.js
+++ public/js/bookmark.js
@@ -1,6 +1,8 @@
 async function toggleBookmark(courseId) {
   
     const bookmarkButton = document.querySelector(`.Course-Bookmark[data-course-id="${courseId}"]`);
+    const scrollPosition = window.pageYOffset; // Store the current scroll position
+
     if (bookmarkButton.classList.contains('bookmarked')) {
       // Remove bookmark
       const response = await fetch('/removeBookmark', {
@@ -10,6 +12,8 @@ async function toggleBookmark(courseId) {
       });
       if (response.ok) {
         bookmarkButton.classList.toggle('bookmarked');
+        // localStorage.setItem('scrollPosition', scrollPosition); // Store the scroll position in local storage
+        location.reload(); // Reload the page after remove
       } else {
         console.error('Failed to remove bookmark');
       }
@@ -28,3 +32,10 @@ async function toggleBookmark(courseId) {
     }
   }
   
+  // window.addEventListener('load', () => {
+  //   const scrollPosition = localStorage.getItem('scrollPosition');
+  //   if (scrollPosition) {
+  //     window.scrollTo(0, parseInt(scrollPosition));
+  //     localStorage.removeItem('scrollPosition'); // Clear the stored scroll position
+  //   }
+  // });
