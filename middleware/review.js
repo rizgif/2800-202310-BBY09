141165627b19b3a80ee1ad19e0f6e5ce7efc2aa6
Heesun Lee <heesun-task@gmail.com>
2023-05-25 15:56:25 -0700

    Add comments on js files under js folder and middleware folder

--- middleware/review.js
+++ middleware/review.js
@@ -1,12 +1,15 @@
+// Import required modules
 require('dotenv').config();
 const Joi = require("joi");
 require("../utils.js");
 
+// Define error messages
 const errorMessages = {
-    emptyReview: 'please provide a review',
+    emptyReview: 'Please provide a review',
     emptyRating: 'Please provide a rating for each category'
 }
 
+// Define review validation schema using Joi
 const reviewScheme = Joi.object({
     review: Joi.string().min(2).max(256).required(),
     courseContentSliderValue: Joi.required(),
@@ -15,7 +18,18 @@ const reviewScheme = Joi.object({
     studentSupportSliderValue: Joi.required(),
 });
 
+/**
+ * Middleware function for validating review data.
+ * Validates the review and rating values in the request body using the reviewScheme.
+ * Sets an error message if validation fails or if any input or rating is empty.
+ * Calls the next middleware function if validation succeeds.
+ * @author Chelsea Yang
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {function} next - The next middleware function.
+ */
 const reviewValidation = async (req, res, next) => {
+    // Extract the review and rating values from the request body
     let review = req.body.review;
     let courseContentSliderValue = req.body.courseContentSliderValue;
     let courseStructureSliderValue = req.body.courseStructureSliderValue;
@@ -29,29 +43,28 @@ const reviewValidation = async (req, res, next) => {
 
     let errorMessage = '';
 
-    console.log(validationResult);
-    // if there is an empty input or error
+    // Check if there is an empty input or validation error
     if (validationResult.error != null) {
         if (review.length < 1) {
+            // Set the error message to indicate an empty review
             errorMessage = errorMessages.emptyReview;
-        } else if (courseContentSliderValue == null) {
-            errorMessage = errorMessages.emptyRating;
-        } else if (courseStructureSliderValue == null) {
-            errorMessage = errorMessages.emptyRating;
-        } else if (teachingStyleSliderValue == null) {
-            errorMessage = errorMessages.emptyRating;
-        } else if (studentSupportSliderValue == null) {
+        } else if (courseContentSliderValue == null ||
+          courseStructureSliderValue == null ||
+          teachingStyleSliderValue == null ||
+          studentSupportSliderValue == null) {
+            // Set the error message to indicate empty ratings for each category
             errorMessage = errorMessages.emptyRating;
         }
 
+        // Set the error message in the request object
         req.errorMessage = errorMessage;
-
-        // return res.status(400).json({ error: errorMessage });
     }
 
+    // Call the next middleware function
     next();
 }
 
+// Export the reviewValidation function
 module.exports = {
     reviewValidation
 }
--- public/js/search-list.js
+++ public/js/search-list.js
@@ -1,15 +1,28 @@
+/**
+ * Event listener for the window scroll event.
+ * Adds or removes the "show" class to the scroll-up button based on the scroll position.
+ * @author Yerim Moon
+ */
 window.addEventListener('scroll', function() {
+    // Find the scroll-up button element with the ID "searchList-scrollUp"
     var button = document.getElementById('searchList-scrollUp');
+
     if (button) {
+        // Check the scroll position
         if (window.pageYOffset > 200) {
+            // Add the "show" class to the button
             button.classList.add('show');
         } else {
+            // Remove the "show" class from the button
             button.classList.remove('show');
         }
     }
 });
 
+/**
+ * Scrolls the window to the top with a smooth behavior.
+ * @author Yerim Moon
+ */
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-  
